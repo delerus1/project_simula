7daa61c5ec8c1054cb7f0e99d09209d7cb25ea5f
Lars Erik Ødegaard <larserikodegaard@Larss-MacBook-Pro.local>
2018-08-15 13:31:40 -0700

    Finnished the refference model, and started on ischemia models

--- multi_perfusion_adv_diff2.py
+++ multi_perfusion_adv_diff2.py
@@ -73,7 +73,7 @@ pressure = new_pressure
 
 
 # Repeating multiple cycles
-n_sim = 4
+n_sim = 5
 
 new_time = []
 for r in np.linspace(0,n_sim-1,n_sim):
@@ -98,7 +98,7 @@ q1,q2,q3 = TestFunctions(FS)
 v1, v2, v3 = TestFunctions(FSC)
 
 # Define constants
-diff_o2 = 10**-10
+diff_o2 = 10**-5
 
 dt = 0.02
 K1 = Constant(1) #(mm^2)/(kPa*s)
@@ -110,8 +110,8 @@ beta23 = Constant(0.05) #1/(kPa*s)
 D = Constant(diff_o2)
 k = Constant(dt)
 
-R_12 = Constant(0.4) # Rate consentration rate
-R_23 = Constant(0.4)  # Rate consentration rate
+R_12 = Constant(1) # Rate consentration rate
+R_23 = Constant(1)  # Rate consentration rate
 
 
 # Define functions 
@@ -144,7 +144,7 @@ F = -K1 * dot(grad(p1), grad(q1))*dx + -K2 * dot(grad(p2), grad(q2))*dx + -K3 * 
 F2 = ((c1 - c_n1) / k)*v1*dx + dot(v_d1, grad(c1))*v1*dx - D*dot(grad(c1),grad(v1))*dx + \
      ((c2 - c_n2) / k)*v2*dx + dot(v_d2, grad(c2))*v2*dx - D*dot(grad(c2),grad(v2))*dx + \
      ((c3 - c_n3) / k)*v3*dx + dot(v_d3, grad(c3))*v3*dx -D*dot(grad(c3),grad(v3))*dx + \
-     inner((c1+R_12*c_n1),v1)*dx + inner((c2-R_12*c_n1),v2)*dx +inner((c2+R_23*c_n2),v2)*dx +inner((c3-R_23*c_n2),v3)*dx
+     R_12*c1*v1*dx - R_12*c1*v2*dx + R_23*c2*v2*dx - R_23*c2*v3*dx
 #dot(R_12*c_n1,v1)*dx - dot(R_12 * c_n1,v2)*dx + dot(R_23*c_n2,v2)*dx - dot(R_23*c_n2,v3)*dx \
 
 # Setting boundry conditions
